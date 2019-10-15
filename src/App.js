d530e7543591c60dc37d603ee84e863701fc6590
alexviarhey <lexa.vergey@gmail.com>
2019-10-15 14:34:28 +0300

    new hover

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const App = () => {
                 <Skills />
                 <ContactReduxForm onSubmit={onSubmit}/>
                 <Footer />
-            </div>
+        </div>
     )
 };
 
