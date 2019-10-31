8bac71ad9f7796adb7c7782a35494396d31b1a34
alexviarhey <lexa.vergey@gmail.com>
2019-10-31 11:26:23 +0300

    Updates

---
+++ precache-manifest.5536670e82632277799dd7b6763530af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "454d54ca9ca09eb5d910515978925c3f",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "ea535a2d7a0261854a70",
+    "url": "/Portfolio/static/css/main.497cd675.chunk.css"
+  },
+  {
+    "revision": "3c0c96d0dec64a9e24c9",
+    "url": "/Portfolio/static/js/2.505a8c37.chunk.js"
+  },
+  {
+    "revision": "ea535a2d7a0261854a70",
+    "url": "/Portfolio/static/js/main.3fea640a.chunk.js"
+  },
+  {
+    "revision": "934ee6a614c19ac9fd73",
+    "url": "/Portfolio/static/js/runtime-main.6977af04.js"
+  },
+  {
+    "revision": "045050f03aa8613e484dbdc9ec3f2e36",
+    "url": "/Portfolio/static/media/Todolist.045050f0.png"
+  }
+]);
